4be624c2911298faee8db37cd5208540213f7d42
IMAM SODIK <imamsodik00@gmail.com>
2026-02-20 01:15:46 +0700

    af

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrinterApiController;
 
 Route::prefix('printer')->group(function () {
-    Route::post('/printer/login', [PrinterApiController::class, 'login']);
-    Route::get('/printer/documents/{printerCode}', [PrinterApiController::class, 'getDocuments']);
-    Route::get('/printer/download/{id}', [PrinterApiController::class, 'download']);
-    Route::post('/printer/update-status', [PrinterApiController::class, 'updateStatus']);
+    Route::post('/login', [PrinterApiController::class, 'login']);
+    Route::get('/documents/{printerCode}', [PrinterApiController::class, 'getDocuments']);
+    Route::get('/download/{id}', [PrinterApiController::class, 'download']);
+    Route::post('/update-status', [PrinterApiController::class, 'updateStatus']);
 });
